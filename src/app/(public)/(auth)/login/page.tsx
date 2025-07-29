4363b89b4792add2e1075f04590644a01e27eae4
TapLamIT <ndtrg281@gmail.com>
2025-07-29 14:45:18 +0700

    init project tiktok clone

---
+++ src/app/(public)/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/dark-mode-toggle";
+
+export default function LoginPage() {
+    return (
+        <div>
+            Sign up for TikTok
+            <ModeToggle />
+        </div>
+    );
+}
