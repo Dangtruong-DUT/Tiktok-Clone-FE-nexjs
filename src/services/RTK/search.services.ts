5371f25ddfb01306fd22897000b0763b11002180
TapLamIT <ndtrg281@gmail.com>
2025-08-29 15:26:59 +0700

    feat: Enhance search API with infinite query support and improved tag handling

--- src/services/RTK/search.services.ts
+++ src/services/RTK/search.services.ts
@@ -1,4 +1,5 @@
 import baseQueryWithReauth from "@/services/RTK/client";
+import { GetListPostRes } from "@/types/response/post.type";
 import { createApi } from "@reduxjs/toolkit/query/react";
 
 export const SearchApi = createApi({
@@ -8,5 +9,40 @@ export const SearchApi = createApi({
     keepUnusedDataFor: 60,
     refetchOnFocus: false,
     refetchOnReconnect: true,
-    endpoints: (builder) => ({}),
+    tagTypes: ["Posts"],
+    endpoints: (builder) => ({
+        searchPosts: builder.infiniteQuery<GetListPostRes, { q: string }, number>({
+            query: ({ pageParam, queryArg }) => `/search?page=${pageParam}&limit=10&q=${queryArg.q}`,
+            providesTags: (result, error, arg) => {
+                if (result) {
+                    const final = [
+                        ...result.pages.flatMap((page) => {
+                            return page.data.posts.map(({ _id }) => ({
+                                type: "Posts" as const,
+                                id: _id,
+                            }));
+                        }),
+                        { type: "Posts" as const, id: `POST-SEARCH-LIST-${arg.q}` },
+                    ];
+                    return final;
+                }
+                return [{ type: "Posts" as const, id: `POST-SEARCH-LIST-${arg.q}` }];
+            },
+            infiniteQueryOptions: {
+                initialPageParam: 1,
+                getNextPageParam: ({ meta }) => {
+                    if (!meta) return undefined;
+                    const { page, total_pages } = meta;
+                    if (page >= total_pages) return undefined;
+                    return page + 1;
+                },
+                getPreviousPageParam: ({ meta }) => {
+                    if (!meta) return undefined;
+                    const { page } = meta;
+                    if (page <= 1) return undefined;
+                    return page - 1;
+                },
+            },
+        }),
+    }),
 });
